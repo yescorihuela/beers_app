0fb0c0820b5e3ddddaf5eb2e5bd85be18c6f05b4
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2021-12-30 00:28:38 -0300

    Add database connection to server execution

--- beers_app/app/bootstrap.go
+++ beers_app/app/bootstrap.go
@@ -5,19 +5,23 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/yescorihuela/beers_app/config"
 	"github.com/yescorihuela/beers_app/domain"
 	"github.com/yescorihuela/beers_app/services"
+	"gorm.io/gorm"
 )
 
 type Server struct {
 	engine   *gin.Engine
 	httpAddr string
+	database *gorm.DB
 }
 
 func NewServer(host string, port uint) Server {
 	server := Server{
 		engine:   gin.New(),
 		httpAddr: fmt.Sprintf("%s:%d", host, port),
+		database: config.ConnectDatabase(),
 	}
 	server.registerRoutes()
 	return server
@@ -29,6 +33,8 @@ func (s *Server) Run() error {
 }
 
 func (s *Server) registerRoutes() {
-	bh := BeerHandlers{service: services.NewBeerService(domain.NewBeerRepositoryStub())}
+
+	// bh := BeerHandlers{service: services.NewBeerService(domain.NewBeerRepositoryStub())}
+	bh := BeerHandlers{service: services.NewBeerService(domain.NewBeerRepositoryDatabase(s.database))}
 	s.engine.GET("/beers", bh.GetAllBeers)
 }
