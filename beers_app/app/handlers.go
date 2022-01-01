e215f3b0ff2c4f2b79a2a5f8878225ec4ecfe297
Yrvin Escorihuela <yrvin.escorihuela@gmail.com>
2022-01-01 17:18:21 -0300

    Remove key for fullfil with OAS

--- beers_app/app/handlers.go
+++ beers_app/app/handlers.go
@@ -15,7 +15,7 @@ type BeerHandlers struct {
 
 func (bh *BeerHandlers) GetAllBeers(ctx *gin.Context) {
 	beers, _ := bh.service.GetAllBeers()
-	ctx.JSON(http.StatusOK, gin.H{"beers": beers})
+	ctx.JSON(http.StatusOK, beers)
 }
 
 func (bh *BeerHandlers) GetBeer(ctx *gin.Context) {
@@ -28,7 +28,7 @@ func (bh *BeerHandlers) GetBeer(ctx *gin.Context) {
 	if err != nil {
 		ctx.JSON(http.StatusNotFound, nil)
 	}
-	ctx.JSON(http.StatusOK, gin.H{"beer": beer})
+	ctx.JSON(http.StatusOK, beer)
 }
 
 func (bh *BeerHandlers) Create(ctx *gin.Context) {
